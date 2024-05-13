f9f2429b1f3f7fccf1bcafda203d9972ea6ba92e
kozMikhailS1286 <koz.mikhail.s@yandex.ru>
2024-05-13 16:22:53 +0300

    3h12m Matching Utilities

--- src/app/appSlice.ts
+++ src/app/appSlice.ts
@@ -1,47 +1,62 @@
-import {createSlice, PayloadAction, UnknownAction} from "@reduxjs/toolkit";
+import {createSlice, isFulfilled, isPending, isRejected, PayloadAction, UnknownAction} from "@reduxjs/toolkit";
 
 const initialState = {
-  status: "idle" as RequestStatusType,
-  error: null as string | null,
-  isInitialized: false,
+    status: "idle" as RequestStatusType,
+    error: null as string | null,
+    isInitialized: false,
 };
 
 export type AppInitialStateType = typeof initialState;
 export type RequestStatusType = "idle" | "loading" | "succeeded" | "failed";
 
 const slice = createSlice({
-  name: "app",
-  initialState,
-  reducers: {
-    setAppError: (state, action: PayloadAction<{ error: string | null }>) => {
-      state.error = action.payload.error;
+    name: "app",
+    initialState,
+    reducers: {
+        setAppError: (state, action: PayloadAction<{ error: string | null }>) => {
+            state.error = action.payload.error;
+        },
+        setAppStatus: (state, action: PayloadAction<{ status: RequestStatusType }>) => {
+            state.status = action.payload.status;
+        },
+        setAppInitialized: (state, action: PayloadAction<{ isInitialized: boolean }>) => {
+            state.isInitialized = action.payload.isInitialized;
+        },
     },
-    setAppStatus: (state, action: PayloadAction<{ status: RequestStatusType }>) => {
-      state.status = action.payload.status;
-    },
-    setAppInitialized: (state, action: PayloadAction<{ isInitialized: boolean }>) => {
-      state.isInitialized = action.payload.isInitialized;
-    },
-  },
     extraReducers: (builder) => {
-      builder.addMatcher((action: UnknownAction) => {
-              return action.type.endsWith('/pending');
-          },
-          (state, action) => {
-              state.status = "loading"
-          })
-      builder.addMatcher((action: UnknownAction) => {
-          return action.type.endsWith('/rejected');
-      },
-          (state, action) => {
-          state.status = "failed"
-      })
-      builder.addMatcher((action: UnknownAction) => {
-          return action.type.endsWith('/fulfilled');
-      },
-          (state, action) => {
-          state.status = "succeeded"
-      })
+        // builder.addMatcher((action: UnknownAction) => {
+        //         return action.type.endsWith('/pending');
+        //     },
+        //     (state, action) => {
+        //         state.status = "loading"
+        //     })
+        // Рефакторим ниже:
+        builder.addMatcher(isPending(),
+            (state, action) => {
+                state.status = "loading"
+            })
+        // builder.addMatcher((action: UnknownAction) => {
+        //     return action.type.endsWith('/rejected');
+        // },
+        //     (state, action) => {
+        //     state.status = "failed"
+        // })
+        // Рефакторим ниже:
+        builder.addMatcher(isRejected(),
+            (state, action) => {
+                state.status = "failed"
+            })
+        // builder.addMatcher((action: UnknownAction) => {
+        //     return action.type.endsWith('/fulfilled');
+        // },
+        //     (state, action) => {
+        //     state.status = "succeeded"
+        // })
+        // Рефакторим ниже:
+        builder.addMatcher(isFulfilled(),
+            (state, action) => {
+                state.status = "succeeded"
+            })
     }
 });
 
